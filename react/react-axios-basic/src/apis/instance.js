0c7f1aca025042d2fa7586fd5a99f506559934ca
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2022-07-31 22:36:46 +0800

    Update axios example

--- react/react-axios-basic/src/apis/instance.js
+++ react/react-axios-basic/src/apis/instance.js
@@ -2,10 +2,10 @@ import axios from "axios";
 // 在根目錄env定義
 const base = process.env.REACT_APP_URL;
 // baseURL是你API的主要Domain，之後發請求時只要填相對路徑就可以了
+let token = localStorage.getItem("token") || "";
 const apiInstance = axios.create({
   baseURL: base,
-  headers: { "Content-Type": "application/json" },
-  timeout: 20000,
+  timeout: 9000,
 });
 
 // Request Interceptors 請求攔截
@@ -29,6 +29,10 @@ apiInstance.interceptors.response.use(
   function (error) {
     if (error.response) {
       switch (error.response.status) {
+        case 401:
+          console.log("'apis/instance.js'token驗證失敗");
+          // go to 404 page
+          break;
         case 404:
           console.log("'apis/instance.js'你要找的頁面不存在");
           // go to 404 page
@@ -45,22 +49,24 @@ apiInstance.interceptors.response.use(
       alert("'apis/instance.js'網路出了點問題，請重新連線後重整網頁");
       return;
     }
-    console.log(`${error.code},${error.message}`)
-    // Promise.reject()
-    return error;
+    console.log(`${error.code},${error.message}`);
+    return Promise.reject(error);
+    // return error;
   }
 );
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  async login({ user, password }) {
-    const res = await apiInstance.post("/login", {
-      user,
-      password,
-    });
-    // eslint-disable-next-line no-unused-vars
-    const token = res.data.token;
-    return Promise.reject(res.data);
+  async login(data) {
+    try {
+      const res = await apiInstance.post("/login", data);
+      // eslint-disable-next-line no-unused-vars
+      token = res?.data?.token || "";
+      apiInstance.defaults.headers.common["authorization"] = "Bearer " + token;
+      return Promise.resolve(res.data);
+    } catch (error) {
+      return Promise.reject(error);
+    }
   },
   async formDataPOST(url, data, config) {
     try {
@@ -83,10 +89,8 @@ export default {
     try {
       // 修改baseUrl的方式
       // apiInstance({ url: url, baseURL: 'http://new-url.com' })
-      // console.log("apis/instance.js");
-      const res = await apiInstance.get(url, {
-        params,
-      });
+      apiInstance.defaults.headers.common["authorization"] = "Bearer " + token;
+      const res = await apiInstance.get(url, params);
       return res.data;
     } catch (res) {
       return Promise.reject(res);
@@ -94,6 +98,7 @@ export default {
   },
   async POST(...arge) {
     try {
+      apiInstance.defaults.headers.common["authorization"] = "Bearer " + token;
       const res = await apiInstance.post(...arge);
       return res.data;
     } catch (res) {
@@ -102,6 +107,7 @@ export default {
   },
   async PUT(...arge) {
     try {
+      apiInstance.defaults.headers.common["authorization"] = "Bearer " + token;
       const res = await apiInstance.put(...arge);
       return res.data;
     } catch (res) {
@@ -110,6 +116,7 @@ export default {
   },
   async DELETE(url, params) {
     try {
+      apiInstance.defaults.headers.common["authorization"] = "Bearer " + token;
       const res = await apiInstance.delete(url, {
         params,
       });
