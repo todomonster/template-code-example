cd69bbbc6d30239fe9045ab838bcd097f4395add
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2022-08-21 23:22:02 +0800

    Update sql example

--- express/express4/js-excel-to-sql/usage_order_mapping_stores/example.sql
+++ express/express4/js-excel-to-sql/usage_order_mapping_stores/example.sql
@@ -1,16 +1,17 @@
 SELECT *
 FROM coupon_coupon_usages
-WHERE store_id > 0
-    AND used_at > date('2022-04-01 00:00:00')
+WHERE used_at > date('2022-04-01 00:00:00')
     AND used_at < date('2022-05-01 00:00:00')
-    AND store_id = 3
+    -- AND store_id = 3
     AND paytype = 2;
 ;
 SELECT *
-FROM mobile_official
-WHERE 結帳時間 < date('2022-05-1 00:00:00')
-    AND 結帳時間 > date('2022-04-01 00:00:00')
-    AND 店別 = '台北衡陽店';
+FROM coupon_coupon_usages
+WHERE paytype = 2
+AND used_at > date('2022-04-01 00:00:00')
+AND used_at < date('2022-05-01 00:00:00')
+AND (store_id=3 OR store_id=4 OR store_id=5 OR store_id=6 OR store_id=7 OR store_id=8 OR store_id=9 OR store_id=10 OR store_id=11 OR store_id=12 OR store_id=13 OR store_id=14 OR store_id=15)
 ;
+
 -- GET 每月份的 資料筆數
 SELECT COUNT(id) FROM `mobile_official` WHERE 結帳時間 > date('2022-04-01') AND 結帳時間 < date('2022-05-01');
