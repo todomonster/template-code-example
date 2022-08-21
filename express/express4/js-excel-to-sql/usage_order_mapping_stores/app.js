e86b5a1a7cb4bea591192c51055da8293d31ec49
mawarutuco <xul4841268@gmail.com>
2022-08-21 11:34:14 +0800

    增加判斷分配月份日期起迄

--- express/express4/js-excel-to-sql/usage_order_mapping_stores/app.js
+++ express/express4/js-excel-to-sql/usage_order_mapping_stores/app.js
@@ -11,65 +11,65 @@ const testData = async (targetTable, limitDate, distributeConfig) => {
   const 依機率產生時間 = () => {
     let minAndSec = ":" + createRandom(0, 59) + ":" + createRandom(0, 59);
     let randomNum = createRandom(1, 10);
-    if (randomNum <= 5) return createRandom(18, 24) + minAndSec;
-    if (randomNum <= 8) return createRandom(12, 17) + minAndSec;
-    return createRandom(8, 11) + minAndSec;
+
+    //萬波營業時間11-21
+    //機率50%，晚上18-21
+    if (randomNum <= 5) return createRandom(18, 21) + minAndSec;
+    //機率40%，下午13-17
+    if (randomNum <= 9) return createRandom(13, 17) + minAndSec;
+    //機率10%，上午11-12
+    return createRandom(11, 12) + minAndSec;
   };
 
-  const 依月份隨機產生日期 = (month) => {
-    switch (month) {
-      case 1:
-      case 3:
-      case 5:
-      case 7:
-      case 8:
-      case 10:
-      case 12:
-        return createRandom(1, 31);
-      case 4:
-      case 6:
-      case 9:
-      case 11:
-        return createRandom(1, 30);
-      default:
-        return createRandom(1, 28);
-    }
+  const 隨機產生日期 = (start, end) => {
+    return createRandom(start, end);
   };
 
-  const 更新資料庫結帳時間 = async (欲修改月份, 參考資料) => {
+  const 更新資料庫結帳時間 = async (欲修改資料, 參考資料) => {
     const { itemYear, itemID } = 參考資料;
-    let 欲修改日期 = 依月份隨機產生日期(欲修改月份);
+    const { month, start, end } = 欲修改資料;
+    let 欲修改日期 = 隨機產生日期(start, end);
     let 欲修改時間 = 依機率產生時間();
-    console.log(欲修改日期 + 欲修改時間);
+    console.log(欲修改日期 + " " + 欲修改時間);
     await executeSQL(`UPDATE ${targetTable}
-      SET 結帳時間 = '${itemYear}-${欲修改月份}-${欲修改日期} ${欲修改時間}'
+      SET 結帳時間 = '${itemYear}-${month}-${欲修改日期} ${欲修改時間}'
       WHERE id=${itemID};`);
   };
 
   let distributeTarget = [];
   distributeConfig.forEach((item) => {
-    let itemTimeData = new Date(item[0]);
+    let itemTimeDataStart = new Date(item[0]);
+    let itemTimeDataEnd = new Date(item[1]);
     distributeTarget.push({
-      month: itemTimeData.getMonth() + 1, //分配月份
+      month: itemTimeDataStart.getMonth() + 1, //分配月份
+      start: itemTimeDataStart.getDate(), //欲分配月份起
+      end: itemTimeDataEnd.getDate(), //欲分配月份迄
       target: item[2], //欲分配月份最多塞幾筆
       nowQuantity: 0, //欲分配月份已塞幾筆
     });
   });
 
-  data.forEach((item, index) => {
-    let nowIndex = index % distributeTarget.length;
-    let 欲改成此月 = distributeTarget[nowIndex].month;
+  data.map((item, index) => {
+    let distributeLength = distributeTarget.length;
+    if (distributeLength <= 0) {
+      console.log("分配目標數量不足，剩下的不做分配");
+      return;
+    }
+
+    let nowIndex = index % distributeLength;
+    let itemDistribute = distributeTarget[nowIndex];
+    let 欲修改資料 = {
+      month: itemDistribute.month,
+      start: itemDistribute.start,
+      end: itemDistribute.end,
+    };
 
     let itemTimeData = new Date(item.結帳時間);
     let itemYear = itemTimeData.getFullYear();
-    // let itemDate = ("0" + itemTimeData.getDate()).slice(-2);
-    // let itemTime = `${itemTimeData.getHours()}:${itemTimeData.getMinutes()}:00`;
-    // let 參考資料 = { itemYear, itemDate, itemTime, itemID: item.id };
     let 參考資料 = { itemYear, itemID: item.id };
 
-    // 更新資料庫結帳時間(欲改成此月, 參考資料);
+    更新資料庫結帳時間(欲修改資料, 參考資料);
 
-    let itemDistribute = distributeTarget[nowIndex];
     itemDistribute.nowQuantity++;
 
     //假如5月塞的數量>5月目標數量，把5月移除，就不會再塞進5月份
@@ -78,15 +78,16 @@ const testData = async (targetTable, limitDate, distributeConfig) => {
     }
   });
 
-  console.log("done");
+  console.log("執行完畢");
 };
 testData(
   "mobile",
-  ["2022-05-03", "2022-05-4"],
+  ["2022-05-01", "2022-05-10"],
   [
-    ["2022-05-03", "2022-05-04", 100],
-    ["2022-06-01", "2022-06-10", 200],
-    ["2022-07-01", "2022-07-11", 1000],
+    // ["2022-05-01", "2022-05-02", 100],
+    ["2022-09-01", "2022-09-10", 200],
+    ["2022-10-01", "2022-10-10", 2000],
+    ["2022-11-01", "2022-11-10", 2000],
   ]
 );
 //第一個參數 資料表名稱(字串)
