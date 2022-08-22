5eb6be1be39377532db890006ca78a8cc54ee8cf
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2022-08-23 01:19:02 +0800

    Update

--- express/express4/js-excel-to-sql/usage_order_mapping_stores/howManyPeople.js
+++ express/express4/js-excel-to-sql/usage_order_mapping_stores/howManyPeople.js
@@ -1,3 +1,22 @@
+const uniqueCity = [
+  "基隆",
+  "台北",
+  "新北",
+  "桃園",
+  "新竹",
+  "苗栗",
+  "台中",
+  "彰化",
+  "雲林",
+  "嘉義",
+  "台南",
+  "高雄",
+  "屏東",
+  "台東",
+  "宜蘭",
+  "澎湖",
+];
+
 const howManyPeople = (location, toFixed = 2) => {
   const allPeople = 23190064;
   let ans = 0;
@@ -43,7 +62,7 @@ const howManyPeople = (location, toFixed = 2) => {
       ans = 800138;
       break;
     case "台東":
-      ans = 212.891;
+      ans = 212891;
       break;
     case "宜蘭":
       ans = 448971;
@@ -54,7 +73,75 @@ const howManyPeople = (location, toFixed = 2) => {
     default:
       ans = false;
   }
-  if (ans) console.log((ans / allPeople).toFixed(toFixed));
-  else console.log("不存在資料內");
+  if (ans) {
+    return (ans / allPeople).toFixed(toFixed);
+  } else console.log("不存在資料內");
 };
-howManyPeople("臺中",5);
+function getRandArea() {
+  const random = Math.floor(Math.random() * 16);
+  return uniqueCity[random];
+}
+
+const areaMapId = {
+  基隆: [2],
+  台北: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+  新北: [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+  桃園: [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43],
+  新竹: [44, 45, 46, 47, 48],
+  苗栗: [49, 50],
+  台中: [51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 86, 179],
+  彰化: [64, 65, 66, 67],
+  雲林: [68],
+  嘉義: [69],
+  台南: [70, 71, 72, 73],
+  高雄: [74, 75, 76, 77, 78, 178],
+  屏東: [79, 80, 81, 82],
+  台東: [83],
+  宜蘭: [84],
+  澎湖: [85],
+};
+function getRandStoreId(area) {
+  const ids = areaMapId[area];
+  const random = Math.floor(Math.random() * ids.length);
+  return ids[random];
+}
+
+const { users } = require("./usersData");
+const { idMapStore } = require("./coupon-stores/data");
+
+const newUsers = [];
+function getHowManyAreaUser(users) {
+  const userLength = users.length;
+  const cityLoop = [];
+  uniqueCity.forEach((el) => {
+    const getRate = howManyPeople(el, 3);
+    cityLoop.push(Math.floor(getRate * userLength));
+  });
+  cityLoop.push(2607); //32082-29475
+  console.log(cityLoop.reduce((a, b) => a + b));
+
+  let counter = 0;
+  cityLoop.forEach((el, index) => {
+    if (el === 2607) {
+      for (let i = 0; i < 2607; i++) {
+        const area = getRandArea();
+        const store_id = getRandStoreId(area);
+        const name = idMapStore[store_id]["name"];
+        newUsers.push({ user_id: users[counter], area, store_id, name });
+        counter++;
+      }
+      return newUsers;
+    }
+    for (let i = 0; i < el; i++) {
+      const area = uniqueCity[index];
+      const store_id = getRandStoreId(area);
+      const name = idMapStore[store_id]["name"];
+      newUsers.push({ user_id: users[counter], area, store_id, name });
+      counter++;
+    }
+  });
+  return newUsers;
+}
+const data = getHowManyAreaUser(users);
+const { saveFile } = require("../server/helper/save-file");
+saveFile("newUsers.js", JSON.stringify(data));
--- express/express4/js-excel-to-sql/usage_order_mapping_stores/index.js
+++ express/express4/js-excel-to-sql/usage_order_mapping_stores/index.js
@@ -1,14 +1,7 @@
-// todo:
-// 清空 uid
-// 修改花蓮地區店家
-// 加入order_id
-// 備份
-
 // 1.自己產生新的表
+// 2. mapping function
 
 const { executeSQL } = require("./db");
-const { isValidDate } = require("../server/helper/date-parser");
-// (['2022-04-01','2022-05-01'],0.61)
 
 const dbConfig = {
   userTable: "vcarduser",
@@ -22,28 +15,86 @@ function yyyy_mm_dd(dateObj) {
   return date.toISOString().slice(0, 10);
 }
 
+
+function randomDigit() {
+  return Math.floor(Math.random() * Math.floor(2));
+}
+
+function generateRandomBinary(binaryLength) {
+  let binary = "";
+  for (let i = 0; i < binaryLength; ++i) {
+    binary += randomDigit();
+  }
+  return binary;
+}
+
+function genTakenId(arr) {
+  // 產生16進位數字
+  const ans = [];
+  arr.forEach((binaryLength) => {
+    const hex = parseInt(generateRandomBinary(binaryLength * 4) , 2);
+    ans.push(hex.toString(16));
+  });
+  return ans.join('-');
+}
+
+// (tableName,['2022-04-01','2022-05-01'],0.61,10)
 async function 依照設定檔的條件以中鼎訂單生成Coupon券(
   tableName,
   [start, end],
-  useCouponRate
+  useCouponRate,
+  limit = 50000
 ) {
   // 步驟一 依照時間 GET 訂單，訂單已經分成 行動支付和現金 並且確定比例了
   const getOrdersSQL = `SELECT * FROM ${tableName} WHERE ${dbConfig.orderTimeColumn} > date('${start} 00:00:00') AND ${dbConfig.orderTimeColumn} < date('${end} 00:00:00')`;
+  let paytype = 2;
+  if (tableName.indecOf("cash") > -1) {
+    paytype = 1;
+  }
   const data = await executeSQL(getOrdersSQL);
-  const dataLength = data.length;
-  data.forEach(async (element) => {
+
+  for (let i = 0, len = data.length; i < len; i++) {
+    if (i > limit - 1) {
+      break;
+    }
+    const order = data[i];
     // 內埔廣濟店
-    const storeName = element[dbConfig.storeName];
-    const orderTime = yyyy_mm_dd(element[dbConfig.orderTimeColumn]);
+    const storeName = order[dbConfig.storeName];
+    const orderTime = yyyy_mm_dd(order[dbConfig.orderTimeColumn]);
 
-    const oneRandomSQL = `SELECT * FROM ${dbConfig.mappingTable} 
-    WHERE ${dbConfig.createTime} < date('${orderTime} 00:00:00') AND ${dbConfig.storeName}=${storeName} 
-    ORDER BY RAND() LIMIT 1`;
+    const getOneRandomSQL = `SELECT * FROM ${dbConfig.mappingTable} 
+        WHERE create_time < date('${orderTime} 00:00:00') AND store_name=${storeName} 
+        ORDER BY RAND() LIMIT 1`;
 
-    const lineUser = await executeSQL(oneRandomSQL);
-    const {lineId,storId} = lineUser
-    genCoupon(lineId);
-  });
+    const lineUser = await executeSQL(getOneRandomSQL);
+
+    await genCoupon(paytype, lineUser, order, useCouponRate);
+  }
 }
 
-function genCoupon() {}
+async function genCoupon(paytype, lineUser, order, useCouponRate) {
+  const coupon_id = 2;
+  const { user_id, store_id } = lineUser;
+  const used_at = order.結帳時間;
+  const taken_at = used_at - 1;
+  const expired_at = used_at + 1;
+  // get
+  const taken_id = genTakenId([8, 4, 4, 4, 12]);
+  //
+  const createSQL = `INSERT INTO coupon_coupon_usages 
+  (coupon_id, store_id, user_id, taken_at, used_at, taken_id, expired_at, paytype)
+  VALUES (${coupon_id}, ${store_id}, '${user_id}', '${taken_at}', '${used_at}', '${taken_id}', '${expired_at}', ${paytype});`;
+  await executeSQL(createSQL);
+}
+/** 
+  {
+coupon_id =2
+store_id	=3...
+user_id =Uafa2aa54e4f010629f3808ae82d4a948
+taken_at	
+used_at	
+taken_id
+expired_at
+paytype =1 =2
+  },
+  */
