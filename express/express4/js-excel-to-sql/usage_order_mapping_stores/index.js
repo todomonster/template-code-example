018939d3de790b1786984b7000ce5a64179684c9
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2022-08-23 18:03:55 +0800

    Update not-used function

--- express/express4/js-excel-to-sql/usage_order_mapping_stores/index.js
+++ express/express4/js-excel-to-sql/usage_order_mapping_stores/index.js
@@ -16,28 +16,50 @@ const dbConfig = {
   orderTimeColumn: "結帳時間",
 };
 
-依照設定檔的條件以中鼎訂單生成Coupon券(
-  "mobile_official",
-  ["2022-04-01", "2022-05-01"],
-  2,
-  100
-);
+/**
+ * 資料表
+ * 產生券的時間 起 訖
+ * 限制券產生的數量
+ * 使用券的百分比%
+ */
 
-// 隨機產生未使用的
+function main() {
+  const table = ["mobile_official", "cash_official"];
+  依照設定檔的條件以中鼎訂單生成Coupon券(
+    "mobile_official",
+    ["2022-04-01", "2022-05-01"],
+    64
+  );
+  依照設定檔的條件以中鼎訂單生成Coupon券(
+    "mobile_official",
+    ["2022-05-01", "2022-06-01"],
+    65
+  );
+  依照設定檔的條件以中鼎訂單生成Coupon券(
+    "mobile_official",
+    ["2022-06-01", "2022-07-01"],
+    59
+  );
+  依照設定檔的條件以中鼎訂單生成Coupon券(
+    "mobile_official",
+    ["2022-07-01", "2022-08-01"],
+    62
+  );
+  依照設定檔的條件以中鼎訂單生成Coupon券(
+    "mobile_official",
+    ["2022-08-01", "2022-09-01"],
+    61
+  );
+}
 
 async function 依照設定檔的條件以中鼎訂單生成Coupon券(
   tableName,
   [start, end],
-  limit = 50000, //產生多少張 以使用的 coupon
-  usedCouponRate = 100 //產生使用券的機率百分比
+  usedCouponRate = 100, //產生使用券的機率百分比
+  limit = 50000 //產生多少張 以使用的 coupon
 ) {
   // 步驟一 依照時間 GET 訂單，訂單已經分成 行動支付和現金 並且確定比例了
   try {
-    /**
-     *
-     *
-     */
-
     const getOrdersSQL = `SELECT * FROM ${tableName} WHERE ${dbConfig.orderTimeColumn} > date('${start} 00:00:00') AND ${dbConfig.orderTimeColumn} < date('${end} 00:00:00')`;
     let paytype = 2;
     if (tableName.indexOf("cash") > -1) {
@@ -47,16 +69,18 @@ async function 依照設定檔的條件以中鼎訂單生成Coupon券(
     const data = await executeSQL(getOrdersSQL);
     const ordersLength = data.length;
     const couponConfig = getCouponConfig(ordersLength, usedCouponRate);
-    console.log("開始執行設定: ", couponConfig);
-    const saveSQL = [];
+
+    const { usedAmount, notUseAmount } = couponConfig;
+
+    let notUsedLimit = (limit * notUseAmount) / usedAmount - 1;
+
+    let saveSQL = [];
     for (let i = 0; i < ordersLength; i++) {
       if (i > limit - 1) {
         break;
       }
       const order = data[i];
-
       const storeName = order[dbConfig.storeName];
-      // addHours(8, dateObj)
       const orderTimeObj = order[dbConfig.orderTimeColumn];
       const orderTime = dateToString(addHours(8, orderTimeObj));
 
@@ -65,42 +89,84 @@ async function 依照設定檔的條件以中鼎訂單生成Coupon券(
         WHERE create_time < date('${orderTime}') AND store_name='${storeName}'
         ORDER BY RAND() LIMIT 1`;
 
-      const lineUser = await executeSQL(getOneRandomSQL);
+      let lineUser = await executeSQL(getOneRandomSQL);
+      if (lineUser.length === 0) {
+        notUsedLimit++;
+        continue;
+      }
       const timeConfig = genTimeConfig(orderTimeObj);
       // 步驟三 產生 coupon券
+      // console.log(paytype, ...lineUser, timeConfig);
       const sql = await genCoupon(paytype, ...lineUser, timeConfig);
-
-      // if(未使用){
-      //   saveSQL.push(sql);
-      // }
-
       saveSQL.push(sql);
     }
 
+    for (let i = 0; i < notUseAmount; i++) {
+      if (i > notUsedLimit) {
+        break;
+      }
+      const random = Math.floor(Math.random() * notUseAmount);
+      const order = data[random];
+
+      const storeName = order[dbConfig.storeName];
+      const orderTimeObj = order[dbConfig.orderTimeColumn];
+      const orderTime = dateToString(addHours(8, orderTimeObj));
+
+      const getOneRandomSQL = `SELECT * FROM ${dbConfig.mappingTable} 
+    WHERE create_time < date('${orderTime}') AND store_name='${storeName}'
+    ORDER BY RAND() LIMIT 1`;
+
+      const lineUser = await executeSQL(getOneRandomSQL);
+      const timeConfig = genTimeConfig(orderTimeObj);
+
+      const sql = await genCoupon(paytype, ...lineUser, timeConfig, "notUsed");
+      saveSQL.push(sql);
+    }
+    saveSQL = saveSQL.sort(() => (Math.random() > 0.5 ? -1 : 1));
+
     await saveFile("new_coupon_usage.sql", saveSQL.join("\n"));
     console.log("完成建立");
+    console.log("執行設定: ", couponConfig);
   } catch (error) {
     console.log(error);
   }
 }
-
-async function genCoupon(paytype, lineUser, timeConfig) {
+// VALUES (2, 23, 'U18cb760ff3e9e002d02c12d5afb23081', '2022/4/2 12:11:31', '2022/4/2 1:10:00', '7a524534-d274-4a4f-eb7f-d4785067db2e', '2022-04-03 00:00:00', 2);
+async function genCoupon(paytype, lineUser, timeConfig, notUsed) {
   const coupon_id = 2;
-  const { line_id, store_id } = lineUser;
-  const { used_at, taken_at, expired_at } = timeConfig;
+  const { line_id = "U18cb760ff3e9e002d02c12d5afb23081", store_id = 23 } =
+    lineUser;
+  const {
+    taken_at = "2022/4/2 12:11:31",
+    used_at = "2022/4/2 1:10:00",
+    expired_at = "2022-04-03 00:00:00",
+  } = timeConfig;
   const taken_id = genTakenId([8, 4, 4, 4, 12]);
-  const createSQL = `INSERT INTO ${dbConfig.genCouponTable} 
+  let createSQL = `INSERT INTO ${dbConfig.genCouponTable} 
   (coupon_id, store_id, user_id, taken_at, used_at, taken_id, expired_at, paytype)
   VALUES (${coupon_id}, ${store_id}, '${line_id}', '${taken_at}', '${used_at}', '${taken_id}', '${expired_at}', ${paytype});`;
+
+  if (notUsed === "notUsed") {
+    createSQL = `INSERT INTO ${dbConfig.genCouponTable} 
+  (coupon_id, store_id, user_id, taken_at, taken_id, expired_at, paytype)
+  VALUES (${coupon_id}, 0, '${line_id}', '${taken_at}', '${taken_id}', '${expired_at}', ${paytype});`;
+  }
+
   return createSQL;
 }
 
 function getCouponConfig(orderLen, couponUseRate) {
   const notUseRate = 100 - couponUseRate;
-  const calculator = (len, rate) => Math.floor((len * rate) / 100);
+  const calculator = (len, rate) =>
+    Math.floor((orderLen * 100) / couponUseRate);
   return {
-    notUseAmount: calculator(orderLen, notUseRate),
-    usedAmount: calculator(orderLen, couponUseRate),
+    usedPercent: couponUseRate,
+    notUsePercent: notUseRate,
+    usedAmount: orderLen,
+    notUseAmount: Math.floor(
+      (calculator(orderLen, couponUseRate) * notUseRate) / 100
+    ),
+    totalAmount: calculator(orderLen, couponUseRate),
   };
 }
 
@@ -118,10 +184,10 @@ function genTimeConfig(dateObj) {
       randomMin * 60 * 1000 -
       randomSec * 1000
   );
-
+  let used_at = dateToString(dateObj);
   return {
     taken_at: dateToString(date),
-    used_at: dateToString(dateObj),
+    used_at,
     expired_at: getTomorrowMidnight(dateObj), //日期+一天後 取前面就好 +00:00:00
   };
 }
