a578cb22ccbe1dd8c679067fc1f843806ec8bfb1
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2022-12-17 15:58:46 +0800

    update icon

--- vue/myfrees-vue-cli-master/src/view/custom/profile/component/DatePicker.vue
+++ vue/myfrees-vue-cli-master/src/view/custom/profile/component/DatePicker.vue
@@ -3,25 +3,49 @@
     <label class="form-label">營業時間</label>
     <div class="input-list">
       <div
-        class="form-check mb-2"
+        class="form-check mb-2 row"
         v-for="(date, index1) in newData"
         :key="date.weekday"
       >
-        <input type="checkbox" class="form-check-input" :id="date.weekday" />
-        <label class="form-check-label" :for="date.weekday">
-            {{date.weekday}}
-        </label>
-        <button class="btn btn-primary btn-sm mb-2 ml-2" type="button">+</button>
-        <div
-          class="row mb-2"
-          v-for="(time, index) in newTime[index1]"
-          :key="index"
-        >
-          <div class="col-6">
-            <input type="time" class="form-control" placeholder="請選擇時間" v-model="time.start"/>
-          </div>
-          <div class="col-6">
-            <input type="time" class="form-control" placeholder="請選擇時間" v-model="time.end"/>
+        <div class="col-12">
+          <input type="checkbox" class="form-check-input" :id="date.weekday" />
+          <label class="form-check-label" :for="date.weekday">
+            {{ date.weekday }}
+          </label>
+          <img :src="addCircleIcon" alt="" class="icon-btn" />
+        </div>
+        <!-- <button class="btn btn-primary btn-sm mb-2 ml-2" type="button">+</button> -->
+        <div class="col-12">
+          <div
+            class="row mb-2"
+            v-for="(time, index) in newTime[index1]"
+            :key="index"
+          >
+            <div class="col">
+              <div class="d-flex flex-row">
+                <div class="col-6">
+                  <input
+                    type="time"
+                    class="form-control"
+                    placeholder="請選擇時間"
+                    v-model="time.start"
+                  />
+                </div>
+                <div
+                  class="d-flex justify-content-center align-items-center m-2px"
+                >
+                  ~
+                </div>
+                <div class="col-6">
+                  <input
+                    type="time"
+                    class="form-control"
+                    placeholder="請選擇時間"
+                    v-model="time.end"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -31,6 +55,11 @@
 <script>
 import { ref, onMounted, onBeforeMount, computed, watch } from "vue";
 import { errorHandle } from "@/utils/errorHandle";
+// import addCircleIcon from "./image/add-circle-outline"
+// import removeCircleIcon from "./image/remove-circle-outline"
+// Icon
+import addCircleIcon from "@/assets/icon/add-circle-outline.svg";
+import removeCircleIcon from "@/assets/icon/remove-circle-outline.svg";
 
 export default {
   name: "DatePicker",
@@ -106,7 +135,16 @@ export default {
       //   console.log(newData.value);
       //   emit("data", 1);
     });
-    return { newData, newTime };
+    return { newData, newTime, addCircleIcon, removeCircleIcon };
   },
 };
 </script>
+<style lang="scss" scoped>
+.icon-btn {
+  max-width: 20px;
+  margin-left: 5px;
+}
+.m-2px {
+  margin: 2px;
+}
+</style>
