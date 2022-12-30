0e3eb8b11a2c3e2391ee28d5b556d078c243dc9c
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2022-12-30 10:06:27 +0800

    鎖定

--- vue/myfrees-vue-cli-master/src/App.vue
+++ vue/myfrees-vue-cli-master/src/App.vue
@@ -15,10 +15,25 @@ export default {
 </script>
 
 <template>
-  <div id="app">
+  <div id="app" class="no-select">
     <RouterView />
     <Loading />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* https://developer.apple.com/forums/thread/133399 */
+.no-select {
+  -webkit-touch-callout: none; /* Safari Touch */
+  -webkit-user-select: none; /* Webkit */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* Edge*/
+  user-select: none; /* Future-proof*/
+}
+.no-drag {
+  -webkit-user-drag: none;
+  -moz-user-drag: none;
+  -o-user-drag: none;
+  user-drag: none;
+}
+</style>
