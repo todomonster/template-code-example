b7132eb670bcf4971810283ea9f0e6c25dae9d7d
mawarutuco <xul4841268@gmail.com>
2023-01-09 00:20:29 +0800

    Add store 路由和畫面

--- vue/mobile-test/src/router/mode/green.js
+++ vue/mobile-test/src/router/mode/green.js
@@ -1,25 +1,57 @@
 export const green = [
-    {
-      path: "/",
-      component: () => import("@/view/custom-user/store/storeIndex.vue"),
-      redirect: "/view",
-      meta: {
-        requiresAuth: false,
-      },
-      children: [
-        {
-          path: "view",
-          meta: {
-            showHeader: true,
-            showFooter: true,
-            requiresAuth: false,
-          },
-          components: {
-            default: () => import("@/view/custom-user/store/reportView.vue"),
-          },
-        },
-      ],
+  {
+    path: "/store",
+    component: () => import("@/view/custom-user/store/storeIndex.vue"),
+    redirect: "/store/list",
+    meta: {
+      requiresAuth: false,
     },
-  ];
+    children: [
+      {
+        path: "list",
+        meta: {
+          showHeader: true,
+          showFooter: true,
+          requiresAuth: false,
+        },
+        components: {
+          default: () => import("@/view/custom-user/store/listView.vue"),
+        },
+      },
+      {
+        path: ":id",
+        meta: {
+          showHeader: true,
+          showFooter: true,
+          requiresAuth: false,
+        },
+        components: {
+          default: () => import("@/view/custom-user/store/detailView.vue"),
+        },
+      },
+      {
+        path: ":id/applyReward",
+        meta: {
+          showHeader: true,
+          showFooter: true,
+          requiresAuth: false,
+        },
+        components: {
+          default: () => import("@/view/custom-user/store/applyRewardView.vue"),
+        },
+      },
+      {
+        path: "report",
+        meta: {
+          showHeader: true,
+          showFooter: true,
+          requiresAuth: false,
+        },
+        components: {
+          default: () => import("@/view/custom-user/store/reportView.vue"),
+        },
+      },
+    ],
+  },
 
-  
+];
