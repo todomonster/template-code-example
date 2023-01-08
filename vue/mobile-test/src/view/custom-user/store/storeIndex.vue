125504e19ac861df3d2d5f49977b2a8b7c1fc8a7
mawarutuco <xul4841268@gmail.com>
2023-01-08 17:49:11 +0800

    Add 我要回報介面

---
+++ vue/mobile-test/src/view/custom-user/store/storeIndex.vue
@@ -0,0 +1,20 @@
+<script>
+import Header from "@/components/global-user/header/HeaderArea.vue";
+// import Footer from "@/components/global/footer/FooterArea.vue";
+
+export default {
+  setup() {},
+  components: {
+    Header,
+    // Footer,
+  },
+};
+</script>
+
+<template>
+  <Header title="我要回報"/>
+  <router-view />
+  <!-- <Footer v-show="$route.meta.showFooter" /> -->
+</template>
+
+<style lang="scss" scoped></style>
