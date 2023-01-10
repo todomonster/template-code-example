f1ef49ac8dea1a011221c378339431940014dedd
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2023-01-10 10:11:34 +0800

    Add scan page

---
+++ vue/mobile-test/src/view/custom-user/scan/scanIndex.vue
@@ -0,0 +1,23 @@
+<script>
+import Header from "@/components/global-user/header/HeaderArea.vue";
+import Footer from "@/components/global-user/footer/FooterArea.vue";
+import "../style/public.scss";
+
+export default {
+  setup() {},
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+
+<template>
+  <Header />
+  <div class="main">
+    <router-view />
+  </div>
+  <Footer v-show="$route.meta.showFooter" />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ vue/mobile-test/src/view/custom-user/scan/scanView.vue
@@ -0,0 +1,26 @@
+<script>
+import { ref, onBeforeMount } from "vue";
+import { useGlobalStore } from "@/store/global";
+import { errorHandle } from "@/utils/errorHandle";
+
+
+export default {
+  setup() {
+    const globalStore = useGlobalStore();
+    const goto = globalStore.goto;
+
+    onBeforeMount(() => {
+
+    });
+
+    return { goto };
+  },
+
+  components: {},
+};
+</script>
+
+<template>
+  <!-- 內容 -->
+  .
+</template>
