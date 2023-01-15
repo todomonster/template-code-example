4a55d35c4ca92b72418b110ea36c433635e94530
mawarutuco <xul4841268@gmail.com>
2023-01-15 23:13:21 +0800

    打api更新畫面

--- vue/mobile-test/src/view/custom-user/wallet/walletRecord.vue
+++ vue/mobile-test/src/view/custom-user/wallet/walletRecord.vue
@@ -10,13 +10,6 @@ export default {
     const recordListData = ref({});
     const moneyListData = ref({});
     const pointListData = ref({});
-    const queryData = {
-      page: 1,
-      limit: 10,
-      type: "point",
-      // startDate: "2022-11-01",
-      // endDate: "2022-11-02",
-    };
 
     const getDateMethod = async (inputData, api, query) => {
       let tmp = await api(query);
@@ -48,7 +41,7 @@ export default {
         errorHandle(error);
       }
     });
-    return {};
+    return { recordListData, moneyListData, pointListData };
   },
 
   components: {},
@@ -91,142 +84,68 @@ export default {
       <div class="swiper-wrapper">
         <div class="swiper-slide">
           <div class="item-container item-container-3">
-            <div class="d-flex">
-              <div class="col-left">
-                <div class="title">myfree社 福利金回饋 $246</div>
-                <div class="date">2022-11-28 14:45:41</div>
-              </div>
-              <div class="col-right">
-                <div class="green">核準</div>
-              </div>
-            </div>
-            <div class="d-flex">
-              <div class="col-left">
-                <div class="title">myfree社 福利金回饋 $215</div>
-                <div class="date">2022-11-28 14:45:32</div>
-                <div class="note">不想給</div>
-              </div>
-              <div class="col-right">
-                <div class="red">取消</div>
-              </div>
-            </div>
-            <div class="d-flex">
-              <div class="col-left">
-                <div class="title">myfree社 福利金回饋 $354</div>
-                <div class="date">2022-11-28 14:45:28</div>
-              </div>
-              <div class="col-right">
-                <div class="black">待確認</div>
-              </div>
-            </div>
-            <div class="d-flex">
-              <div class="col-left">
-                <div class="title">myfree社 福利金回饋 $425</div>
-                <div class="date">2022-11-28 14:45:22</div>
-                <div class="note">暱稱</div>
-              </div>
-              <div class="col-right">
-                <div class="green">核準</div>
-              </div>
-            </div>
-            <div class="d-flex">
-              <div class="col-left">
-                <div class="title">myfree社 福利金回饋 $324</div>
-                <div class="date">2022-11-28 14:45:17</div>
-                <div class="note">心情不好</div>
-              </div>
-              <div class="col-right">
-                <div class="red">取消</div>
-              </div>
-            </div>
-            <div class="d-flex">
-              <div class="col-left">
-                <div class="title">myfree社 福利金回饋 $205</div>
-                <div class="date">2022-11-28 14:45:13</div>
-              </div>
-              <div class="col-right">
-                <div class="green">核準</div>
-              </div>
-            </div>
-            <div class="d-flex">
-              <div class="col-left">
-                <div class="title">myfree社 福利金回饋 $109</div>
-                <div class="date">2022-11-28 14:44:57</div>
-                <div class="note">備註</div>
-              </div>
-              <div class="col-right">
-                <div class="red">取消</div>
+            <div v-for="data in recordListData" :key="data.createTime">
+              <div class="d-flex">
+                <div class="col-left">
+                  <div class="title">
+                    {{ data.storeName }} 福利金回饋 ${{ data.amount }}
+                  </div>
+                  <div class="date">{{ data.createTime }}</div>
+                  <div v-if="data.status === 2" class="note">
+                    {{ data.memo }}
+                  </div>
+                </div>
+                <div class="col-right">
+                  <div v-if="data.status === 0" class="black">待確認</div>
+                  <div v-else-if="data.status === 1" class="green">核准</div>
+                  <div v-else-if="data.status === 2" class="red">取消</div>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div class="swiper-slide">
           <div class="item-container item-container-3">
-            <div class="d-flex">
-              <div class="col-left">
-                <div class="title">扣除超集點17</div>
-                <div class="date">2022-11-28 14:47:57</div>
-              </div>
-              <div class="col-right">
-                <div class="red">-17</div>
-              </div>
-            </div>
-            <div class="d-flex">
-              <div class="col-left">
-                <div class="title">扣除超集點29</div>
-                <div class="date">2022-11-28 14:47:49</div>
-              </div>
-              <div class="col-right">
-                <div class="red">-29</div>
-              </div>
-            </div>
-            <div class="d-flex">
-              <div class="col-left">
-                <div class="title">扣除超集點14</div>
-                <div class="date">2022-11-28 14:47:32</div>
-              </div>
-              <div class="col-right">
-                <div class="red">-14</div>
-              </div>
-            </div>
-            <div class="d-flex">
-              <div class="col-left">
-                <div class="title">註冊發送300超級點</div>
-                <div class="date">2022-11-25 09:11:09</div>
-              </div>
-              <div class="col-right">
-                <div class="green">300</div>
+            <div v-for="data in moneyListData" :key="data.id">
+              <div class="d-flex">
+                <div class="col-left">
+                  <div class="title">{{ data.name }}</div>
+                  <div class="date">{{ data.createTime }}</div>
+                  <div v-if="data.status === 2" class="note">
+                    {{ data.memo }}
+                  </div>
+                </div>
+                <div class="col-right">
+                  <div v-if="data.status === 1" class="green">
+                    {{ data.amount }}
+                  </div>
+                  <div v-else-if="data.status === 2" class="red">
+                    {{ data.amount }}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div class="swiper-slide">
           <div class="item-container item-container-3">
-            <div class="d-flex">
-              <div class="col-left">
-                <div class="title">取得店家10%回饋金17</div>
-                <div class="date">2022-11-28 14:47:57</div>
-              </div>
-              <div class="col-right">
-                <div class="green">17</div>
-              </div>
-            </div>
-            <div class="d-flex">
-              <div class="col-left">
-                <div class="title">取得店家10%回饋金29</div>
-                <div class="date">2022-11-28 14:47:49</div>
-              </div>
-              <div class="col-right">
-                <div class="green">29</div>
-              </div>
-            </div>
-            <div class="d-flex">
-              <div class="col-left">
-                <div class="title">取得店家10%回饋金14</div>
-                <div class="date">2022-11-28 14:47:32</div>
-              </div>
-              <div class="col-right">
-                <div class="green">14</div>
+            <div v-for="data in pointListData" :key="data.id">
+              <div class="d-flex">
+                <div class="col-left">
+                  <div class="title">{{ data.name }}</div>
+                  <div class="date">{{ data.createTime }}</div>
+                  <div v-if="data.status === 2" class="note">
+                    {{ data.memo }}
+                  </div>
+                </div>
+                <div class="col-right">
+                  <div v-if="data.status === 1" class="green">
+                    {{ data.amount }}
+                  </div>
+                  <div v-else-if="data.status === 2" class="red">
+                    {{ data.amount }}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
