9332c01ee201887d2507116da8c51cd8a602a32c
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2023-01-16 10:33:44 +0800

    Update swipe回到上方,區域高度調高

--- vue/mobile-test/src/view/custom-user/wallet/walletRecord.vue
+++ vue/mobile-test/src/view/custom-user/wallet/walletRecord.vue
@@ -11,16 +11,67 @@ export default {
     const pointListData = ref({});
     const moneyListData = ref({});
 
+    const observerTarget = ref('{"a":null,"b":null,"c":null}');
+
     const getDateMethod = async (inputData, api, query) => {
-      let tmp = await api(query);
-      if (tmp.result) {
-        inputData.value = tmp.data;
+      let response = await api(query);
+      if (response.result) {
+        inputData.value = response.data;
       }
     };
+
+    const observer = (DOMid) => {
+      // https://thatcatinsomnia.medium.com/%E6%AA%A2%E6%9F%A5element%E6%98%AF%E5%90%A6%E5%87%BA%E7%8F%BE%E5%9C%A8%E7%95%AB%E9%9D%A2%E4%B9%8B%E4%B8%AD-check-if-an-element-gets-visible-in-the-screen-during-scrolling-57a9230ea843
+      const target = document.getElementById(DOMid);
+      const options = {
+        // 四個數值分別表示上、右、下、左
+        rootMargin: "0px 0px 0px 0px",
+      };
+
+      const onIntersection = (entries) => {
+        const target = JSON.parse(observerTarget.value);
+        entries.forEach((entry) => {
+          target[DOMid] = entry.isIntersecting ? 1 : 0;
+          observerTarget.value = JSON.stringify(target);
+        });
+      };
+
+      const observer = new IntersectionObserver(onIntersection, options);
+
+      observer.observe(target);
+    };
+
+    watch(
+      () => observerTarget.value,
+      (newQuestion, oldQuestion) => {
+        if (newQuestion.indexOf("null") > -1) {
+          return;
+        }
+        const falg =
+          (newQuestion == '{"a":1,"b":1,"c":0}' &&
+            oldQuestion == '{"a":1,"b":1,"c":1}') ||
+          newQuestion == '{"a":1,"b":1,"c":1}' ||
+          (newQuestion == '{"a":0,"b":1,"c":1}' &&
+            oldQuestion == '{"a":1,"b":1,"c":1}');
+
+        if (falg) {
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "instant",
+          });
+        }
+      }
+    );
 
     onMounted(async () => {
       initSwiper(3);
       try {
+        // 開始監聽 如果有改變swiper位置的話滾動到最上方
+        observer("a");
+        observer("b");
+        observer("c");
+
         getDateMethod(recordListData, apiGetUserRecordList, {
           page: 1,
           limit: 10,
@@ -41,7 +92,7 @@ export default {
         errorHandle(error);
       }
     });
-    return { recordListData, moneyListData, pointListData };
+    return { recordListData, moneyListData, pointListData, observerTarget };
   },
 
   components: {},
@@ -53,14 +104,15 @@ export default {
   <section class="c-main">
     <div class="swiper-container ui-tab">
       <div class="swiper-wrapper">
-        <div class="swiper-slide"><span>申請紀錄</span></div>
-        <div class="swiper-slide"><span>超集點</span></div>
-        <div class="swiper-slide"><span>福利金</span></div>
+        <div class="swiper-slide" @click="toTop"><span>申請紀錄</span></div>
+        <div class="swiper-slide" @click="toTop"><span>超集點</span></div>
+        <div class="swiper-slide" @click="toTop"><span>福利金</span></div>
       </div>
     </div>
     <div class="swiper-container ui-page">
       <div class="filter-containter">
-        <div class="row">
+        <!-- 先不做此功能 -->
+        <!-- <div class="row">
           <div class="col">
             <a href="javascript:void(0);" class="filter-link active">今日</a>
           </div>
@@ -79,11 +131,11 @@ export default {
           <div class="col">
             <a href="javascript:void(0);" class="filter-link">1年</a>
           </div>
-        </div>
+        </div> -->
       </div>
       <div class="swiper-wrapper">
         <div class="swiper-slide">
-          <div class="item-container item-container-3">
+          <div class="item-container item-container-3" id="a">
             <div v-for="data in recordListData" :key="data.createTime">
               <div class="d-flex">
                 <div class="col-left">
@@ -105,7 +157,7 @@ export default {
           </div>
         </div>
         <div class="swiper-slide">
-          <div class="item-container item-container-3">
+          <div class="item-container item-container-3" id="b">
             <div v-for="data in pointListData" :key="data.id">
               <div class="d-flex">
                 <div class="col-left">
@@ -125,7 +177,7 @@ export default {
           </div>
         </div>
         <div class="swiper-slide">
-          <div class="item-container item-container-3">
+          <div class="item-container item-container-3" id="c">
             <div v-for="data in moneyListData" :key="data.id">
               <div class="d-flex">
                 <div class="col-left">
@@ -148,3 +200,8 @@ export default {
     </div>
   </section>
 </template>
+<style lang="scss" scoped>
+.ui-page {
+  height: calc(100vh - $header-height - 15px);
+}
+</style>
