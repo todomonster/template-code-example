d8fddb416c23c9e3ba26e3af94a16c03d1e7a5cf
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2023-02-06 15:21:48 +0800

    Update 列印串接連結

--- vue/mobile-test/src/view/custom/setting/qrCode.vue
+++ vue/mobile-test/src/view/custom/setting/qrCode.vue
@@ -2,19 +2,38 @@
 import { ref, onMounted } from "vue";
 import { apiGenQrcode, apiGetStore } from "@/api/myfree";
 import { errorHandle } from "@/utils/errorHandle";
+import { apiPrintStoreQRcode } from "@/api/myfree";
 
 import { ExtCall } from "@/utils/extCall";
+import { Toast } from "@/components/global/swal";
+import { useGlobalStore } from "@/store/global";
 
 export default {
   name: "QrCode",
   setup() {
+    const globalStore = useGlobalStore();
+    const goto = globalStore.goto;
     const qrcodeUrl = ref("因為意外無法正確轉換");
+
+    let storeName = "";
+    let id = null;
+    const btnDisable = ref(false);
+
     onMounted(async () => {
       try {
         const store = await apiGetStore();
-        const id = store.id || 0;
-        const response = await apiGenQrcode({ storeId: id });
-        qrcodeUrl.value = response;
+        id = store?.id;
+        // 讓id=0以及其他數字通過
+        if (typeof id !== "number" && id < 0) {
+          btnDisable.value = true;
+          Toast("取得店家id失敗");
+          return;
+        }
+        storeName = store?.name;
+        const responseUrl = await apiGenQrcode({ storeId: id });
+        if (responseUrl) {
+          qrcodeUrl.value = responseUrl;
+        }
       } catch (error) {
         errorHandle(error);
       }
@@ -29,13 +48,17 @@ export default {
         aTag.href = canvasToDataUrl("qrCanvas");
         aTag.download = "myfrees_store_qrcode.png";
 
+        let message = `${storeName}邀請您加入myFrees會員，立即加入得300點，消費立即索取福利金，點數換現金。`;
+        if (qrcodeUrl.value != "因為意外無法正確轉換") {
+          message += ` 連結: ${qrcodeUrl.value}`;
+        }
+
         // 手機版是呼叫分享
         ExtCall.shareFile({
           subject: "share",
           title: "store_qr_code",
+          message,
           base64: aTag.href,
-          message:
-            "${店家}邀請您加入myFrees會員，立即加入得300點，消費立即索取福利金，點數換現金。",
         });
       } catch (error) {
         // 電腦版是下載
@@ -45,7 +68,14 @@ export default {
       }
     };
 
-    return { qrcodeUrl, download };
+    const handlePrint = () => {
+      // open /api/admin/v1/download/files?storeId=40
+      let QR_DOWNLOAD_URL = process.env.VUE_APP_STORE_QR_DOWNLOAD_URL;
+      if (id) QR_DOWNLOAD_URL += id;
+      goto("toBrowser", QR_DOWNLOAD_URL);
+    };
+
+    return { qrcodeUrl, download, btnDisable, handlePrint };
   },
   components: {},
 };
@@ -55,12 +85,9 @@ export default {
   <div class="main-content">
     <div class="row text-center">
       <div class="col">
-        <vue-qrcode :value="qrcodeUrl" :options="{ width: 325 }"></vue-qrcode>
-      </div>
-      <div class="col custom-hide">
         <vue-qrcode
           :value="qrcodeUrl"
-          :options="{ width: 189 }"
+          :options="{ width: 325 }"
           id="qrCanvas"
         ></vue-qrcode>
       </div>
@@ -71,6 +98,7 @@ export default {
           class="btn btn-primary custom-primary w-75 custom-max-325"
           type="button"
           @click="download"
+          :disabled="btnDisable"
         >
           <i class="fa fa-share-alt me-1" aria-hidden="true"></i>
           分享
@@ -82,6 +110,8 @@ export default {
         <button
           class="btn btn-primary custom-primary w-75 custom-max-325"
           type="button"
+          @click="handlePrint"
+          :disabled="btnDisable"
         >
           <i class="fa fa-print me-1" aria-hidden="true"></i>
           列印
