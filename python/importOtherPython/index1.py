865bd44abd534d3303523130c44cceff46191c68
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2023-05-20 22:32:49 +0800

    Update import example

---
+++ python/importOtherPython/index1.py
@@ -0,0 +1,5 @@
+from index2 import greet, add_numbers
+
+greet("Alice")
+result = add_numbers(2, 3)
+print(result)
---
+++ python/importOtherPython/index2.py
@@ -0,0 +1,9 @@
+test_int1 = 1
+global test_int2
+test_int2 = 2
+
+def greet(name):
+    print("test_int1 " + str(test_int1) , " test_int2 " + str(test_int2))
+
+def add_numbers(a, b):
+    return a + b
