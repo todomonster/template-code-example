1034845af3757967d4d36b3daee80a959c3dd363
Richard Lin <33016870+dpes8693@users.noreply.github.com>
2023-09-04 12:19:01 +0800

    Update 測試 google api 編輯 excel

--- express/google/sheet/env.js
+++ express/google/sheet/env.js
@@ -1,5 +1,5 @@
 // 開啟指定的 Google Sheets 工作表
 // https://docs.google.com/spreadsheets/d/{spreadsheetId}/edit#gid=0
 // 記得開啟編輯權限，否則會403
-const spreadsheetId = '';
+const spreadsheetId = '1BU1l4EEJhn6C1FGihOZ5mjElt84h2qWMTlS0sNrVKGc';
 module.exports = { spreadsheetId };
